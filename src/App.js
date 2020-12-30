0b2f3176f9f48020f8b01037cf9ecc5f70a3503d
chirayurathi <chirayurathi4@gmail.com>
2020-12-30 19:55:18 +0530

    pincode

--- src/App.js
+++ src/App.js
@@ -23,9 +23,24 @@ class App extends Component {
       Youtube:'',
       CustomerProfile:'',
       Search:'',
-      comments:''
+      comments:'',
+      Pincode:'',
+      PersonalEmail:''
     },
-    submitted:false
+    submitted:false,
+    error:{
+      email:false,
+      BusinessName:false,
+      BusinessAddress:false,
+      PAN:false,
+      PersonName:false,
+      PhoneNumber:false,
+      Categories:false,
+      BusinessType:false,
+      CustomerProfile:false,
+      Search:false,
+      Pincode:false
+    }
   }
   changeHandler = (e,tag)=>{
     let copy = {...this.state.form}
@@ -34,15 +49,41 @@ class App extends Component {
   }
   submitHandler = (e)=>{
     e.preventDefault()
-    axios.post(`https://esowcarpet-default-rtdb.firebaseio.com/form.json`, this.state.form )
-    .then(res => {
-      console.log(res);
-      console.log(res.data);
-      this.setState({submitted:true})
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
+    let flag = false
+    let errorCopy = {...this.state.error}
+    let keys = Object.keys(errorCopy)
+    for(let i = 0; i < keys.length; i++){
+      if(this.state.form[keys[i]]===""){
+        errorCopy[keys[i]] = true
+      }
+      else{
+        errorCopy[keys[i]] = false
+      }
+    }
+    if((this.state.form.Pincode === "600001" || this.state.form.Pincode === "600079" || this.state.form.Pincode === "600003")){
+      errorCopy["Pincode"] = false 
+     }
+     else{
+       errorCopy["Pincode"] = true
+     }
+    for(let i = 0; i < keys.length; i++){
+      if(errorCopy[keys[i]]){
+        flag = true
+      }
+    }
+    this.setState({error:errorCopy})
+    if(!flag){
+      axios.post(`https://esowcarpet-default-rtdb.firebaseio.com/form.json`, this.state.form )
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.setState({submitted:true})
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    }
+    
     console.log(this.state.form)
   }
   render(){
@@ -50,7 +91,7 @@ class App extends Component {
       <div className="App">
         <Nav/>
         <h1>Shop Registration</h1>
-        {this.state.submitted?<div><h1>Your Response has been submitted</h1></div>:<Form form={this.state.form} submit={this.submitHandler} changeHandler={this.changeHandler}/>}
+        {this.state.submitted?<div><h1>Your Response has been submitted</h1></div>:<Form error={this.state.error} form={this.state.form} submit={this.submitHandler} changeHandler={this.changeHandler}/>}
       </div>
     );
   }
