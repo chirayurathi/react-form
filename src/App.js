1e67837e725bfe78696a9433ea15fbe32e0f6e7e
chirayurathi <chirayurathi4@gmail.com>
2020-12-30 12:03:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Nav from './Nav/Nav';
+import Form from './Form/Form'
+import React, { Component } from 'react'
+import axios from 'axios'
+class App extends Component {
+  state = {
+    form:{
+      email:'',
+      BusinessName:'',
+      BusinessAddress:'',
+      PAN:'',
+      GST:'',
+      PersonName:'',
+      PhoneNumber:'',
+      Landline:'',
+      BusinessWhatsApp:'',
+      Categories:'',
+      BusinessType:'',
+      Website:'',
+      Facebook:'',
+      Instagram:'',
+      Youtube:'',
+      CustomerProfile:'',
+      Search:'',
+      comments:''
+    },
+    submitted:false
+  }
+  changeHandler = (e,tag)=>{
+    let copy = {...this.state.form}
+    copy[tag] = e.target.value
+    this.setState({form:copy})
+  }
+  submitHandler = (e)=>{
+    e.preventDefault()
+    axios.post(`https://esowcarpet-default-rtdb.firebaseio.com/form.json`, this.state.form )
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+      this.setState({submitted:true})
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+    console.log(this.state.form)
+  }
+  render(){
+    return (
+      <div className="App">
+        <Nav/>
+        <h1>Shop Registration</h1>
+        {this.state.submitted?<div><h1>Your Response has been submitted</h1></div>:<Form form={this.state.form} submit={this.submitHandler} changeHandler={this.changeHandler}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
